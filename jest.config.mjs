bd4ed84973a2be9162829df0ceaa858e43d7ee70
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-06-07 06:15:23 -0500

    Increase coverage tracking

--- jest.config.mjs
+++ jest.config.mjs
@@ -9,6 +9,9 @@ export default {
     '<rootDir>/config/db.js',
     '<rootDir>/routes/**/*.js',
     '<rootDir>/controllers/**/*.js',
+    '<rootDir>/models/**/*.js',
+    '<rootDir>/middleware/**/*.js',
   ],
+  coverageDirectory: '<rootDir>/coverage',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
