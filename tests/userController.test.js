3477e765993bcc454b9790f6e5a44c9b7c67606f
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-06-14 19:21:39 -0500

    Add new tests

--- tests/userController.test.js
+++ tests/userController.test.js
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import app from '../server.js';
 import User from '../models/User.js';
+import FriendRequest from '../models/FriendRequest.js';
 import { connectDB, disconnectDB, clearDB, createUser } from '../config/db.js';
 
 beforeAll(async () => {
@@ -55,5 +56,69 @@ describe('User Profile Routes', () => {
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty('firstName', 'Jane');
     expect(res.body).toHaveProperty('lastName', 'Doe');
+  });
+});
+
+describe('Check Friend Request Status', () => {
+  let user, otherUser, token;
+
+  beforeEach(async () => {
+    user = await createUser({
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@example.com',
+      password: 'password123',
+    });
+
+    otherUser = await createUser({
+      firstName: 'Jane',
+      lastName: 'Doe',
+      email: 'jane@example.com',
+      password: 'password123',
+    });
+
+    const res = await request(app).post('/auth/login').send({
+      email: 'john@example.com',
+      password: 'password123',
+    });
+
+    token = res.body.token;
+  });
+
+  it('should return pending if a friend request is already sent', async () => {
+    await FriendRequest.create({
+      requester: user._id,
+      recipient: otherUser._id,
+    });
+
+    const res = await request(app)
+      .get(`/users/friend-request-status/${user._id}/${otherUser._id}`)
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('status', 'pending');
+  });
+
+  it('should return friends if users are already friends', async () => {
+    user.friends.push(otherUser._id);
+    otherUser.friends.push(user._id);
+    await user.save();
+    await otherUser.save();
+
+    const res = await request(app)
+      .get(`/users/friend-request-status/${user._id}/${otherUser._id}`)
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('status', 'friends');
+  });
+
+  it('should return none if no friend request is sent and users are not friends', async () => {
+    const res = await request(app)
+      .get(`/users/friend-request-status/${user._id}/${otherUser._id}`)
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('status', 'none');
   });
 });
