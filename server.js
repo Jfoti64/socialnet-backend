9e727438b1a74896aa74e98a479623fd1442edbf
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-06-16 04:22:28 -0500

    Don't use rate limiter on tests

--- server.js
+++ server.js
@@ -21,12 +21,14 @@ const compression = require('compression');
 const app = express();
 
 // Set up rate limiter: maximum of 30 requests per minute
-const RateLimit = require('express-rate-limit');
-const limiter = RateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
-  max: 30,
-});
-app.use(limiter);
+if (process.env.NODE_ENV !== 'test') {
+  const RateLimit = require('express-rate-limit');
+  const limiter = RateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 30,
+  });
+  app.use(limiter);
+}
 
 app.use(compression()); // Compress all routes
 
